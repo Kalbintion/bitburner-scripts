d58761419085b1f81e9cee74c91cdcf55d5bfd98
Kalbintion Kier <distantimage24@gmail.com>
2025-05-30 18:18:48 -0500

    Update hacknet.js

--- hacknet.js
+++ hacknet.js
@@ -1,43 +1,58 @@
 import { COLORS } from "colors";
 
-/** @param {NS} ns */
+const FLAGS = [['maxServers', 18], ['numLevels', 10], ['numRam', 1], ['numCores', 1], ['sleepTime', 1000]];
+
+/**
+ * Manages the hacknet node upgrading & buying process
+ * @param {Number} maxServers   Maximum number of nodes to buy
+ * @param {Number} numLevels    How many levels to buy each attempt
+ * @param {Number} numRam       How many ram levels to buy each attempt
+ * @param {Number} numCores     How many core levels to buy each attempt
+ * @param {Number} sleepTime    Time between script checking to buy
+ */
 export async function main(ns) {
-  let hn = ns.hacknet;
-  let numOfServers = hn.numNodes();
+  const flags = ns.flags(FLAGS);
+
+  let numOfServers = ns.hacknet.numNodes();
 
   while (true) {
     let playerMoney = ns.getServerMoneyAvailable("home");
 
     for (var i = 0; i < numOfServers; i++) {
-      let nodeInfo = hn.getNodeStats(i);
+      let nodeInfo = ns.hacknet.getNodeStats(i);
 
-      let levelCost = hn.getLevelUpgradeCost(i, 10);
-      let ramCost = hn.getRamUpgradeCost(i, 1);
-      let coreCost = hn.getCoreUpgradeCost(i, 1);
+      let levelCost = ns.hacknet.getLevelUpgradeCost(i, 10);
+      let ramCost = ns.hacknet.getRamUpgradeCost(i, 1);
+      let coreCost = ns.hacknet.getCoreUpgradeCost(i, 1);
 
       if (levelCost < ramCost && levelCost < coreCost && levelCost < playerMoney) {
         ns.tprint(COLORS.BRIGHT_CYAN + "Buying 10 levels for hacknet node " + i + COLORS.RESET);
-        hn.upgradeLevel(i, 10);
+        ns.hacknet.upgradeLevel(i, flags.numLevels);
         playerMoney = ns.getServerMoneyAvailable("home");
       } else if (ramCost < levelCost && ramCost < coreCost && ramCost < playerMoney) {
         ns.tprint(COLORS.BRIGHT_CYAN + "Buying ram for hacknet node " + i + COLORS.RESET);
-        hn.upgradeRam(i, 1);
+        ns.hacknet.upgradeRam(i, flags.numRam);
         playerMoney = ns.getServerMoneyAvailable("home");
       } else if (coreCost < ramCost && coreCost < levelCost && coreCost < playerMoney) {
         ns.tprint(COLORS.BRIGHT_CYAN + "Buying core upgrade for hacknet node " + i + COLORS.RESET);
-        hn.upgradeCore(i, 1);
+        ns.hacknet.upgradeCore(i, flags.numCores);
         playerMoney = ns.getServerMoneyAvailable("home");
       }
     }
 
-    let nodeCost = hn.getPurchaseNodeCost();
+    let nodeCost = ns.hacknet.getPurchaseNodeCost();
 
-    if (nodeCost < playerMoney) {
+    if (nodeCost < playerMoney && numOfServers <= flags.maxServers) {
       let newSvIdx = hn.purchaseNode();
       ns.tprint(COLORS.BRIGHT_CYAN + "Bought new hacknet node: " + newSvIdx);
-      numOfServers = hn.numNodes();
+      numOfServers = ns.hacknet.numNodes();
     }
 
-    await ns.sleep(1000);
+    await ns.sleep(flags.sleepTime);
   }
+}
+
+export function autocomplete(data, args) {
+  data.flags(FLAGS);
+  return [];
 }
