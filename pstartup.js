c0053296d7ddaade83d139b264a76a8e97d83230
Kalbintion Kier <distantimage24@gmail.com>
2025-06-03 19:05:45 -0500

    Update pstartup.js

--- pstartup.js
+++ pstartup.js
@@ -4,13 +4,17 @@ import { COLORS } from "colors";
 export async function main(ns) {
   var serverList = ns.getPurchasedServers();
   let scriptName = ns.args[0] || "pserv.js";
+  let target = ns.args[1] || "";
+  let delayBetween = ns.args[2] || 0;
 
   for (var i = 0; i < serverList.length; ++i) {
     const servName = serverList[i];
 
     ns.killall(servName);
     ns.scp(["colors.js", "servers.js", scriptName], servName);
-    ns.exec(scriptName, servName, Math.floor(ns.getServerMaxRam(servName) / ns.getScriptRam(scriptName)));
-    ns.tprint(COLORS.WHITE + "INFO: Setup server: " + servName + COLORS.RESET);
+    ns.exec(scriptName, servName, Math.floor(ns.getServerMaxRam(servName) / ns.getScriptRam(scriptName)), target);
+    ns.tprintf("%s", COLORS.WHITE + "Setup server: " + servName + COLORS.RESET);
+    await ns.sleep(delayBetween);
   }
+
 }
