a8b59b9e2b4f8db6181365bfae8d312bfc8b46e2
Kalbintion Kier <distantimage24@gmail.com>
2025-06-03 19:06:51 -0500

    Update pstartup-home.js

--- pstartup-home.js
+++ pstartup-home.js
@@ -1,18 +1,20 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  let delayBetween = ns.args[0] || 0;
+  let reservedRAM = ns.args[0] || 64;
+  let numThreads = ns.args[1] || 300;
+  let delayBetween = ns.args[2] || 0;
 
   ns.kill("phome.js", "home");
   let homeRAM = ns.getServerMaxRam("home");
   let freeRAM = homeRAM - ns.getServerUsedRam("home");
 
   let pSize = ns.getScriptRam("phome.js", "home");
-  let pSizeTotal = pSize * 300;
+  let pSizeTotal = pSize * numThreads;
 
   ns.tprintf("Home RAM: %s, Free RAM: %s, Script Size: %s", homeRAM, freeRAM, pSize);
-  while (freeRAM - pSizeTotal >= 64) {
+  while (freeRAM - pSizeTotal >= reservedRAM) {
     ns.tprint("Spawned phome.js");
-    ns.exec("phome.js", "home", 300);
+    ns.exec("phome.js", "home", numThreads);
     homeRAM = ns.getServerMaxRam("home");
     freeRAM = homeRAM - ns.getServerUsedRam("home");
     await ns.sleep(delayBetween);
