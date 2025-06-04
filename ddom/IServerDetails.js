47a816b6660d9e960e903b84e78ea8cb063e50d4
Kalbintion Kier <distantimage24@gmail.com>
2025-06-04 04:10:25 -0500

    Create IServerDetails.js

---
+++ ddom/IServerDetails.js
@@ -0,0 +1,59 @@
+import { SecurityData } from "./SecurityData.js";
+import { TimingData } from "./TimingData.js";
+import { PortData } from "./PortData.js";
+import { GrowthData } from "./GrowthData.js";
+import { RamData } from "./RamData.js";
+
+export class IServerDetails {
+  constructor() {
+    // Constructor logic here, if any
+  }
+
+  endItAll() {
+    throw new Error("Method 'endItAll' not implemented.");
+  }
+
+  refresh() {
+    throw new Error("Method 'refresh' not implemented.");
+  }
+
+  fetchSecurity() {
+    throw new Error("Method 'fetchSecurity' not implemented.");
+  }
+
+  fetchTiming() {
+    throw new Error("Method 'fetchTiming' not implemented.");
+  }
+
+  fetchPorts() {
+    throw new Error("Method 'fetchPorts' not implemented.");
+  }
+
+  fetchGrowth() {
+    throw new Error("Method 'fetchGrowth' not implemented.");
+  }
+
+  fetchRAM() {
+    throw new Error("Method 'fetchRAM' not implemented.");
+  }
+
+  hackable() {
+    throw new Error("Method 'hackable' not implemented.");
+  }
+
+  async hack() {
+    throw new Error("Method 'hack' not implemented.");
+  }
+
+  get value() {
+    throw new Error("Getter 'value' not implemented.");
+  }
+
+  transfer(files, src, dest) {
+    throw new Error("Method 'transfer' not implemented.");
+  }
+
+  hasFiles(file) {
+    throw new Error("Method 'hasFiles' not implemented.");
+  }
+}
