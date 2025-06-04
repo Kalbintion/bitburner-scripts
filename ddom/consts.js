ee2a83f88881622013f0b7fbb3fefdd5b3f9512c
Kalbintion Kier <distantimage24@gmail.com>
2025-06-04 00:20:35 -0500

    Update consts.js

--- ddom/consts.js
+++ ddom/consts.js
@@ -5,7 +5,25 @@ export const FILES = {
   sql: "SQLInject.exe",
   smtp: "relaySMTP.exe",
   nuke: "NUKE.exe",
-  slaveWeak: "/ddom/slave-weak.js",
-  slaveHack: "/ddom/slave-hack.js",
-  slaveGrow: "/ddom/slave-grow.js"
+  DDOM: { // Distributed Denial of Money
+    master: "/ddom/master.js",
+    slaveWeak: "/ddom/slave-weak.js",
+    slaveHack: "/ddom/slave-hack.js",
+    slaveGrow: "/ddom/slave-grow.js",
+
+    serverDetails: "/ddom/ServerDetails.js",
+    serverHacker: "/ddom/ServerHacker.js",
+    serverListing: "/ddom/ServerListing.js",
+
+    boxes: "/ddom/boxes.js",
+    logger: "/ddom/logger.js",
+    consts: "/ddom/consts.js"
+  }
+}
+
+export const BB_CONSTS = {
+  TIME_PER_TICK: 200,
+  SEC_PER_HACK_THREAD: 0.002,
+  SEC_PER_GROW_THREAD: 0.004,
+  SEC_REDUCE_PER_WEAKEN_THREAD: 0.05
 }
