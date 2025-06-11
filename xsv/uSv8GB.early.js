736a035a5d96d220b769098686b3119203ff6560
Kalbintion Kier <distantimage24@gmail.com>
2025-06-11 02:26:46 -0500

    Reverted uSv8GB to its original state before it was overwritten accidentally

--- xsv/uSv8GB.early.js
+++ xsv/uSv8GB.early.js
@@ -2,5 +2,23 @@ import {COLORS} from "colors";
 
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.exec("uSvNGB.js", "home", 1, "size 8");
+  let serverList = ns.getPurchasedServers();
+  let pMoney = ns.getServerMoneyAvailable("home");
+
+  for(var i = 0; i < serverList.length; ++i) {
+    let hostname = serverList[i];
+    let uCost = ns.getPurchasedServerUpgradeCost(hostname, 16);
+    let maxRam = ns.getServerMaxRam(hostname);
+
+    if(uCost < pMoney && maxRam == 8) {
+      ns.tprint(COLORS.BRIGHT_YELLOW + "Upgraded server " + hostname + " to 16GB");
+      ns.upgradePurchasedServer(hostname, 16);
+      pMoney = ns.getServerMoneyAvailable("home");
+    } else if(maxRam > 8) {
+      ns.tprint(COLORS.YELLOW + "Skipped server " + hostname + ", already upgraded.");
+    } else {
+      ns.tprint(COLORS.BRIGHT_RED + "Failed to upgrade " + hostname + ", costs: " + uCost + ", have: " + pMoney);
+      return;
+    }
+  }
 }
